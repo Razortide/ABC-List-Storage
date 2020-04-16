8b61995f1d48eaf4e6bd9b8636548a395367298d
razortide <marc.keil@posteo.de>
2020-04-16 23:30:45 +0200

    Starting Menu Atomization of methods (display List, enterValue, displayTopics)

--- ABCList.py
+++ ABCList.py
@@ -6,27 +6,39 @@ import time
 def cls():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-
 class ABCList:
     """Stores a single ABC list"""
-    topicsCount = {"total": 0}
+    total_count = 0
+    topicsCount = {}
+
+    def displayList(self):
+        print("Thema: {}   Nummer: {}   Datum: {}   Zeit: {}"
+              .format(self.topic, self.number, self.date, self.time))
+        for key in self.content_list:
+            print("{} - {}".format(key, self.content_list[key]))
+
+    def enterValue(self, entry):
+        if entry[0].upper() not in self.content_list.keys():
+            self.content_list.update({entry[0]: []})
+        if entry not in self.content_list[entry[0].upper()]:
+            self.content_list[entry[0].upper()].append(entry)
 
     def __init__(self):
         self.topic = input("Thema der ABC Liste: ")
-        date = input("Datum der ABC Liste (DD. MM. JJJJ): ")
+        date = input("Datum der ABC Liste (DD.MM.JJJJ): ")
         if date == "":
-            self.date = "{}. {}. {}".format(datetime.date.today().day, datetime.date.today().month,
-                                            datetime.date.today().year)
+            self.date = "{}.{}.{}".format(datetime.date.today().day, datetime.date.today().month,
+                                          datetime.date.today().year)
         else:
             self.date = date
-        print("Setting date to {}".format(self.date))
-        ABCList.topicsCount["total"] += 1
+        print("Setze den {} als Datum.".format(self.date))
+        ABCList.total_count += 1
         if self.topic not in ABCList.topicsCount:
-            print("Creating {} as new topic".format(self.topic))
+            print("Erstelle {} als neues Thema".format(self.topic))
             ABCList.topicsCount[self.topic] = 1
         else:
             ABCList.topicsCount[self.topic] += 1
-            print("Creating the {}. list regarding the topic {}".format(ABCList.topicsCount[self.topic], self.topic))
+            print("Erstelle die {}. Liste zum Thema '{}'".format(ABCList.topicsCount[self.topic], self.topic))
 
         self.content_list = {'A': [], 'B': [], 'C': [], 'D': [], 'E': [], 'F': [], 'G': [], 'H': [], 'I': [], 'J': [],
                              'K': [], 'L': [], 'M': [], 'N': [], 'O': [], 'P': [], 'Q': [], 'R': [], 'S': [], 'T': [],
@@ -37,15 +49,10 @@ class ABCList:
         self.consolidated = False  # Is used to determine if list was already consolidated before
         start_time = time.time()
         while True:
-            print("Thema: {}   Nummer: {}   Datum: {}   Zeit: {}".format(self.topic, self.number, self.date, self.time))
-            for key in self.content_list:
-                print("{} - {}".format(key, self.content_list[key]))
+            self.displayList()
             entry = input("Begriff: ")
             if entry != "":
-                if entry[0] not in self.content_list.keys():
-                    self.content_list.update({entry[0]: []})
-                if entry not in self.content_list[entry[0].upper()]:
-                    self.content_list[entry[0].upper()].append(entry)
+                self.enterValue(entry)
                 self.time = time.time() - start_time
                 cls()
             else:
@@ -54,19 +61,25 @@ class ABCList:
     def editABCList(self):
         # Nur Änderungen möglich
         while True:
-            print("Thema: {}   Nummer: {}   Datum: {}   Zeit: {}".format(self.topic, self.number, self.date, self.time))
-            for key in self.content_list:
-                print("{} - {}".format(key, self.content_list[key]))
-            entry = input("Begriff (oder EXIT): ")
-            if entry != "EXIT":
+            self.displayList()
+            entry = input("Begriff: ")
+            if entry != "":
                 if entry in self.content_list[entry[0].upper()]:
-                    pass
-                    # TODO: Änderung implementieren
+                    self.content_list[entry[0].upper()].remove(entry)
+                    print("{} ausgewählt.".format(entry))
+                    self.enterValue(input("Neuen Begriff eingeben: "))
+                else:
+                    print("Begriff '{}' nicht in ABC-Liste vorhanden. Bitte auf Groß- und Kleinschreibung achten."
+                          .format(entry))
                 cls()
             else:
                 break
 
     @classmethod
+    def displayTopics(cls):
+        print (cls.topicsCount.keys())
+        #TODO: Ausgabe aufhübschen
+
     def consolidateTopic(cls, topic):
         pass
         # Todo: Konsolidiert ABC-Listen eines Topics
